cd50a49ea1b0d064eb0d5e49761ff0b251a0cc07
Robertbaelde <robert_baelde@hotmail.com>
2022-03-27 20:52:00 +0100

    Implement BackoffStrategy

--- src/WaitingForLockRepository.php
+++ src/WaitingForLockRepository.php
@@ -2,30 +2,32 @@
 
 namespace Robertbaelde\ProjectionEngine;
 
+use EventSauce\BackOff\BackOffStrategy;
+use EventSauce\BackOff\ExponentialBackOffStrategy;
+use Throwable;
+
 class WaitingForLockRepository implements ProjectionEngineLockRepository
 {
     private int $tries = 0;
+    private BackOffStrategy $backOff;
 
     public function __construct(
         protected ProjectionEngineLockRepository $lockRepository,
-        protected int $sleepInMicroseconds = 1000,
-        protected int $maxAmountOfTries = 2
+        BackOffStrategy $backOff = null,
     )
     {
+        $this->backOff = $backOff ?: new ExponentialBackOffStrategy(10000, 25);
     }
 
     public function lockForHandlingMessages(string $aggregateRootId): void
     {
         startOfRequiringLock:
         try {
-            $this->lockRepository->lockForHandlingMessages($aggregateRootId);
-        } catch (ConsumerIsLockedByOtherProcess $locked)
-        {
             $this->tries++;
-            if($this->tries > $this->maxAmountOfTries){
-                throw new ConsumerIsLockedByOtherProcess();
-            }
-            usleep($this->sleepInMicroseconds);
+            $this->lockRepository->lockForHandlingMessages($aggregateRootId);
+        } catch (Throwable $throwable)
+        {
+            $this->backOff->backOff($this->tries, $throwable);
             goto startOfRequiringLock;
         }
     }
